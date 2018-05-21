d1e619bf4bab39a5f31766c49408dea5ab2f37cd
haydonR <haydon.ruth19@gmail.com>
2018-05-20 21:11:12 -0400

    Added Ranged Attack Arm
    Added an arm that rotates an arm that is attached to the player.

--- Assets/Scripts/Player Scripts/AnimationController.cs
+++ Assets/Scripts/Player Scripts/AnimationController.cs
@@ -9,7 +9,7 @@ public class AnimationController : MonoBehaviour {
     Animator animator;
     SpriteRenderer spriteRenderer;
     //Need access to PlayerController Functions to know what the player is doing.
-
+    public GameObject armRotation;
     
     // Use this for initialization
     void Start () {
@@ -29,6 +29,34 @@ public class AnimationController : MonoBehaviour {
         {
             transform.rotation = Quaternion.Euler(0, 180f, 0);
 
+        }
+
+        //Shows sprite with pistol out
+        if(player.playerState == PlayerState.RangedAttack)
+        {
+            armRotation.SetActive(true);
+            //input is used to read the direction of the right analog stick
+            Vector3 input = new Vector3(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical"), 0.0f);
+            //angle is used to determine the angle in which the right analog stick is being held
+            var angle = Mathf.Atan2(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical")) * Mathf.Rad2Deg -90;
+            print("Angle: " + angle);
+            if (angle <= 90 || angle >= -90)
+            {
+                print("Less than 90, and GREATER THAN -90");
+                armRotation.transform.rotation = Quaternion.Euler(0, 0, angle);
+            }
+            else
+            {
+                //Rotate Player when aiming behind
+                print("THIS HAPPENED! Player should be facing left.");
+                transform.rotation = Quaternion.Euler(0, 180f, 0);
+                //Rotate the animation for the gun on the Y-axis
+                armRotation.transform.rotation = Quaternion.Euler(0, 180f, angle);
+            }
+        }
+        else
+        {
+            armRotation.SetActive(false);
         }
     }
 
---
+++ Assets/Scripts/Player Scripts/ArmRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 input = new Vector3(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical"), 0.0f);
+        var angle = Mathf.Atan2(Input.GetAxis("RHorizontal"), Input.GetAxis("RVertical")) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, (angle - 90));
+	}
+}
